d2443ee6cf36f67e0c064c4ddf2ba93ae312872a
Michael Shaughnessy <mickeyshaughnessy@gmail.com>
2015-03-08 22:29:00 -0700

    forked Insight repo and added my code

--- run.sh
+++ run.sh
@@ -1,17 +1,29 @@
 #!/usr/bin/env bash
 
-# example of the run script for running the word count
-
 # first I'll load all my dependencies
-apt-get install python-pandas
+#apt-get install python-pandas
+sudo pip install mrjob
+sudo apt-get install mrjob
 
 # next I'll make sure that all my programs (written in Python in this example) have the proper permissions
 chmod a+x my_word_count.py
 chmod a+x my_running_median.py
 
 # finally I'll execute my programs, with the input directory wc_input and output the files in the directory wc_output
-python ./src/my_word_count.py ./wc_input ./wc_output/wc_result.txt
-python ./src/my_running_median.py ./wc_input ./wc_output/med_result.txt
+python my_running_median.py med_result.txt wc_input/*
+python my_word_count.py --output-dir wc_output/ wc_input/*
+sed 's/"//g' wc_output/part-00000 > wc_output/wc_result.txt
+rm wc_output/part-00000
+### if multiple part* files are being produced, use this syntax instead of the line above:
+#--------------------
+# sed 's/"//g' wc_output/part-00000 > wc_output/wc_result.txt   # use the first to make the wc_result.txt file
+# rm wc_output/part-00000
+# FILES= wc_output/part*
+# for f in $FILES
+# do
+#  echo "Processing $f file..."
+#  sed 's/"//g' $f >> wc_output/wc_result.txt   # use the first to make the wc_result.txt file
+#-----------------------------------------------
 
 
 
